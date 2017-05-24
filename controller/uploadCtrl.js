c62ff9fd6defaed2756b4f634d631b6105582dae
intvnet <intvnet@hanmir.com>
2017-05-24 14:30:44 +0900

    ext

--- controller/uploadCtrl.js
+++ controller/uploadCtrl.js
@@ -7,22 +7,40 @@ app.controller("uploadCtrl",function ($http,$scope,$rootScope,$state){
         var uploadForm=document.getElementById('uploadForm');
         var formData=new FormData(uploadForm);
         //formData.append("fileUpload",uploadForm.uploadFile.files[0]);
-        console.log(uploadForm.uploadFile.files[0]);
+        var uploadFile=uploadForm.uploadFile.files[0];
+        var fileName=uploadFile.name;
 
-        $http({
-         url:$rootScope.aipUrl+"/api/fileUpload/test",
-         method:"POST",
-         headers: {'Content-Type':undefined},
-         transformRequest: angular.identity,
-         data:formData,
-         withCredentials: true
-         }).success(function(data,status){
-            alert(status);
-            console.log(data);
-         }).error(function(data,status){
-            alert("error");
-            console.log(data)
-         });
+        //console.log(fileName.length);
+        //console.log("라스트인덱스"+fileName.lastIndexOf("."));
+
+
+        if(fileName.lastIndexOf(".") != -1){
+            var fileExt=fileName.substring(fileName.lastIndexOf(".")+1,fileName.length).toLowerCase();
+            if(fileExt=="xlsx" || fileExt=="xls" || fileExt=="csv"){
+                $http({
+                    url:$rootScope.aipUrl+"/api/fileUpload/test",
+                    method:"POST",
+                    headers: {'Content-Type':undefined},
+                    transformRequest: angular.identity,
+                    data:formData,
+                    withCredentials: true
+                }).success(function(data,status){
+                    alert(status);
+                    console.log(data);
+                }).error(function(data,status){
+                    alert("error");
+                    console.log(data)
+                });
+            }else{
+                alert("please insert xlsx,xls,csv file!!");
+            }
+        }else{
+            alert("no ext!!");
+        }
+
+
+
+
 
     }
 });
